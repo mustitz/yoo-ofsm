a0844d67530256045cc675b6b3a043c4791e94db
Andrii Sevastianov <mustitz@gmail.com>
2016-01-27 22:15:57 +0100

    T-00063: Add “parse_command_line” function stub.
    Function “parse_command_line”  will parse command line. Surprise!
    Not this is empty function which returns OK.
    
    Changes to be committed:
    	modified:   source/common.c

--- source/common.c
+++ source/common.c
@@ -1,6 +1,16 @@
 #include <fsm.h>
 
+static int parse_command_line(int argc, char * argv[])
+{
+    return 0;
+}
+
 int execute(int argc, char * argv[], build_script_func f)
 {
+    int errcode = parse_command_line(argc, argv);
+    if (errcode != 0) {
+        return errcode;
+    }
+
     return 0;
 }
