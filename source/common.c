585f00e6c8c33b51afd57e58af5665b3ecd0747f
avs <andrey.sevastyanov@shopit.se>
2016-01-28 14:29:09 +0100

    T-00063: Add logic for script create/free.
    Also “run” and “script” stubs was added. Now function “execute” performs
    a little bit more actions.
    
    Changes to be committed:
    	modified:   source/common.c

--- source/common.c
+++ source/common.c
@@ -1,9 +1,19 @@
 #include <fsm.h>
 
+#include <yoo-stdlib.h>
+
 #include <stdarg.h>
 #include <stdio.h>
 
-#define errheader errmsg("Error in function “%s”, file %s:%d.", __FUNCTION__, __FILE__, __LINE__)
+#define STATUS__NEW           1
+#define STATUS__EXECUTING     2
+#define STATUS__FAILED        3
+#define STATUS__INTERRUPTED   4
+#define STATUS__DONE          5
+
+
+
+#define ERRHEADER errmsg("Error in function “%s”, file %s:%d.", __FUNCTION__, __FILE__, __LINE__)
 
 static void verrmsg(const char * fmt, va_list args)
 {
@@ -20,17 +30,39 @@ static void errmsg(const char * fmt, ...)
     va_end(args);
 }
 
+
+
 struct script
 {
+    struct mempool * restrict mempool;
+    int status;
 };
 
 struct script * create_script()
 {
-    return NULL;
+    struct mempool * restrict mempool = create_mempool(4000);
+    if (mempool == NULL) {
+        ERRHEADER;
+        errmsg("create_mempool(4000) failed with NULL as return value.");
+        return NULL;
+    }
+
+    struct script * restrict script = mempool_alloc(mempool, sizeof(struct script));
+    if (script == NULL) {
+        ERRHEADER;
+        errmsg("mempool_alloc(mempool, %lu) failed with NULL as return value.", sizeof(struct script));
+        free_mempool(mempool);
+        return NULL;
+    }
+
+    script->mempool = mempool;
+    script->status = STATUS__NEW;
+    return script;
 }
 
-void free_script(struct script * restrict script)
+void free_script(struct script * restrict me)
 {
+    free_mempool(me->mempool);
 }
 
 static int parse_command_line(int argc, char * argv[])
@@ -38,10 +70,18 @@ static int parse_command_line(int argc, char * argv[])
     return 0;
 }
 
+static void run(struct script * restrict me)
+{
+}
+
+static void print(struct script * restrict me)
+{
+}
+
 int execute(int argc, char * argv[], build_script_func build)
 {
     if (build == NULL) {
-        errheader;
+        ERRHEADER;
         errmsg("Invalid argument “f” = NULL (build script function). Not NULL value is expected.");
         return 1;
     }
@@ -53,8 +93,19 @@ int execute(int argc, char * argv[], build_script_func build)
 
     struct script * restrict script;
     script = create_script();
+    if (script == NULL) {
+        ERRHEADER;
+        errmsg("create_script failed with NULL value. Terminate script execution.");
+        return 1;
+    }
 
     build(script);
+
+    run(script);
+
+    if (script->status == STATUS__DONE) {
+        print(script);
+    }
 
     free_script(script);
     return 0;
