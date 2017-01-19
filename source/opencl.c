1481898642547b5eb4b8b35d23ef79748a52d46a
Andrii Sevastianov <andrii.sevastianov@globallogic.com>
2017-01-19 17:16:12 +0200

    T-00136: Fix compiling in no OpenCL mode.
    Function stubs was fixed.
    
    Changes to be committed:
    	modified:   source/opencl.c

--- source/opencl.c
+++ source/opencl.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
+#include <yoo-stdlib.h>
+#include <poker.h>
+
 #ifdef HAS_OPENCL
 
 #include <CL/cl.h>
-
-#include <yoo-stdlib.h>
-#include <poker.h>
 
 struct opencl_state
 {
@@ -713,18 +713,19 @@ int run_opencl_permutations(
     void * handle,
     uint64_t qdata,
     const uint64_t * const data,
-    uint16_t * restrict const report
+    uint16_t * restrict const report,
+    uint64_t * restrict const qerrors
 )
 {
     return 1;
 }
 
 int opencl__test_permutations(
-    const int32_t n, const uint32_t start_state, const uint32_t qdata,
-    const int8_t * const perm_table, const uint64_t perm_table_sz,
-    const uint32_t * const fsm, const uint64_t fsm_sz,
-    const uint64_t * const data, const uint64_t data_sz,
-    uint16_t * restrict const report, const uint64_t report_sz
+    uint64_t * restrict args,
+    const int8_t * const perm_table, const int64_t perm_table_sz,
+    const uint32_t * const fsm, const int64_t fsm_sz,
+    const uint64_t * const data, const int64_t data_sz,
+    uint16_t * restrict const report, const int64_t report_sz
 )
 {
     printf("[FAIL]\n");
